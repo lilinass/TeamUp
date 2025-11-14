3e43b379a8a61898e3388f2df8315d2e62f87e48
Sara JI-LO <sara.ji-lo@esme.fr>
2025-11-14 09:06:31 +0100

    rajout page

---
+++ Front/scriptest.js
@@ -0,0 +1,12 @@
+<script>
+  const pass = document.getElementById("password");
+  const confirm = document.getElementById("password-confirm");
+
+  confirm.addEventListener("input", () => {
+    if (confirm.value !== pass.value) {
+      confirm.style.borderColor = "#ff6b6b";
+    } else {
+      confirm.style.borderColor = "#2b3346";
+    }
+  });
+</script>
