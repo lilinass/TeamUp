c094a66d80ce4837c866ec302384933df9541c60
Aurélie <nass.aurelie@gmail.com>
2025-11-14 11:57:29 +0100

    Modification page principale

--- Front/scriptest.js
+++ Front/scriptest.js
@@ -1,12 +1,10 @@
-<script>
-  const pass = document.getElementById("password");
-  const confirm = document.getElementById("password-confirm");
+const pass = document.getElementById("password");
+const confirm = document.getElementById("password-confirm");
 
-  confirm.addEventListener("input", () => {
-    if (confirm.value !== pass.value) {
-      confirm.style.borderColor = "#ff6b6b";
-    } else {
-      confirm.style.borderColor = "#2b3346";
-    }
-  });
-</script>
+confirm.addEventListener("input", () => {
+  if (confirm.value !== pass.value) {
+    confirm.style.borderColor = "#ff6b6b";
+  } else {
+    confirm.style.borderColor = "#2b3346";
+  }
+});
